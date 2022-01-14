3cd459264d6b5170cc5fb443b350dc1081840217
Terry <terryluan12@gmail.com>
2022-01-14 04:03:52 -0500

    added comments and removed/refactored redundant variables

--- file-transfer-app/deliver.c
+++ file-transfer-app/deliver.c
@@ -8,56 +8,78 @@
 
 #define MAXBUFLEN 100
 
+/*
+ * Function: deliver <server address> <server port number>
+ * -------------------------------------------------------
+ * Given the port and IPv4 address, allows a user to 
+ * do various actions. Only supports ftp at the moment
+ */
+
 int main(int argc, char *argv[]){
 
     struct addrinfo hints, *servinfo;
     struct in_addr servaddr;
-    char input[MAXBUFLEN], output[MAXBUFLEN];
+    char buf[MAXBUFLEN];
     char ftppt[3] = "ftp";
     int numbytes;
 
-
+    // only works if the user gives 2 arguments
     if (argc != 3){
         printf("NOT ENOUGH ARGUMENTS\n");
         return -1;
     }
+
+    // set the hints struct to use IPv4, UDP, and 
+    // accept a numeric port
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_NUMERICSERV;
 
+    // try to convert the server address from "printable"
+    // form to "network" form. Store it in servaddr
     int success = inet_pton(AF_INET, argv[1], &servaddr);
 
+    // if it's successful, notify hint that the IP address
+    // is numeric
     if(success == 1){
         hints.ai_flags |= AI_NUMERICHOST;
     }
 
+    // get the info of the server and store in servinfo
     success = getaddrinfo(argv[1], argv[2], &hints, &servinfo);
-
     if(success != 0){
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(success));
         printf("COULDN'T FIND ADDRESS: %s AND PORT %s\n", argv[1], argv[2]);
         return -1;
     }
 
+    // try to get the socket info based on info of server
     int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
     if(sockfd == -1){
         printf("COULDN'T FIND SOCKET");
         return -1;
     }
 
+    // go into a continuous loop as commands can be input
     while(1){
         printf("Please enter ftp <file> or -1 to exit: ");
-        scanf("%s", input);
-        if(strcmp(input, ftppt) != 0){
-            printf("first command must be ftp. Cannot be %st\n", input);
+        scanf("%s", buf);
+
+        // check if first word input is "ftp". If not, exit.
+        if(strcmp(buf, ftppt) == 0){
+            printf("first command must be ftp. Cannot be %st\n", buf);
             return -1;
-        }else if(input == "-1"){
+        }else if(buf == "-1"){
             break;
         }
-        scanf("%s", input);
-        if(access(input, F_OK) == 0){
-            if((numbytes = sendto(sockfd, ftppt, 3, 0, servinfo->ai_addr, servinfo->ai_addrlen)) == -1){
+
+        scanf("%s", buf);
+
+        // if the file exists, then send "ftp" to the server. Otherwise quit
+        if(access(buf, F_OK) == 0){
+            numbytes = sendto(sockfd, ftppt, 3, 0, servinfo->ai_addr, servinfo->ai_addrlen);
+            if(numbytes == -1){
                 perror("COULDN'T SEND\n");
                 return -1;
             }
@@ -68,14 +90,15 @@ int main(int argc, char *argv[]){
         printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
         printf("Waiting on response...\n");
 
-        numbytes = recvfrom(sockfd, &output, MAXBUFLEN-1, 0, servinfo->ai_addr, (socklen_t *) &servinfo->ai_addrlen);
-
-        output[numbytes] = '\0';
+        // wait for a response
+        numbytes = recvfrom(sockfd, &buf, MAXBUFLEN-1, 0, servinfo->ai_addr, (socklen_t *) &servinfo->ai_addrlen);
+        buf[numbytes] = '\0';
 
         printf("listener: got packet, %d bytes long\n", numbytes);
-        printf("Got a %s\n", output);
+        printf("Got a %s\n", buf);
 
-        if(strcmp(output, "yes") == 0){
+        // check response
+        if(strcmp(buf, "yes") == 0){
             printf("A file transfer can start.\n");
         }else{
             printf("File transfer cannot occur\n");
--- file-transfer-app/server.c
+++ file-transfer-app/server.c
@@ -8,30 +8,45 @@
 
 #define MAXBUFLEN 256
 
+
+/*
+ * Function: server <UDP listen port>
+ * -------------------------------------------------------
+ * Given the port and IPv4 address, allows a user to 
+ * do various actions. Only supports ftp at the moment
+ */
+
 int main(int argc, char *argv[]){
     char buf[MAXBUFLEN];
     struct addrinfo hints, *servinfo;
     struct sockaddr_storage their_addr;
     socklen_t their_addr_len;
-    int port, rv;
+    int success;
 
+    // one argument must be provided
     if(argc != 2){
         printf("MUST BE ONE ARG");
         return -1;
     }
-
+    // set the hints struct to use IPv4, UDP, and 
+    // use default IP address
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_PASSIVE;
     
-    if((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0){
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    // get the info of the server and store in servinfo
+    success = getaddrinfo(NULL, argv[1], &hints, &servinfo);
+    if(success != 0){
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(success));
         return -1;
     }
 
+    // get socket info
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
+    // bind socket so that the port is associated with
+    // the specific IP address
     if(bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
         close(sockfd);
         printf("ERROR IN BIND\n");
@@ -41,9 +56,10 @@ int main(int argc, char *argv[]){
     while(1){
         printf("listener: waiting to recvfrom...\n");
 
+        // wait to receive something from user. If received
+        // store users IP address
         int numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1, 0,
                                 (struct sockaddr *)&their_addr, &their_addr_len);
-
         if(numbytes == -1){
             printf("NOTHING RECEIVED");
             close(sockfd);
@@ -55,7 +71,8 @@ int main(int argc, char *argv[]){
         printf("listener: got packet, %d bytes long\n", numbytes);
         printf("Got a %s\n", buf);
 
-
+        // if the server gets "ftp", send a "yes" back. 
+        // else send back "no"
         if(strcmp(buf, "ftp") == 0){
             numbytes = sendto(sockfd, "yes", 3, 0, (struct sockaddr *)&their_addr, their_addr_len);
         }else{
